4c39eeb6c9002e83f985a54231687a3f572887fc
Prithviraj2511 <prithvirajpatil2511@gmail>
2025-02-24 23:18:20 +0530

    added small image in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="/images/avatar-1.jpg"
+                src="/images/picture2.jpeg"
                 width={40}
                 height={40}
                 alt="potrait"
