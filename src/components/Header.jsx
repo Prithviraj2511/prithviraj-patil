314cbd01dd23406fe4f8abd76702869dc87d5224
Prithviraj2511 <prithvirajpatil2511@gmail>
2025-03-08 21:50:23 +0530

    fixing image not loading on github pages issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>
           <a href="/" className="logo">
             <img
-              src="/images/logo.svg"
+              src="./images/logo.svg"
               width={40}
               height={40}
               alt="Prithviraj Patil"
